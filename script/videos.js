55faa60070e09aa054444b0948d5dd96421a4f03
Lazaro Alves <lazaroalves12355@gmail.com>
2022-12-03 15:20:32 -0300

    first commit

---
+++ script/videos.js
@@ -0,0 +1,42 @@
+export default [
+    {
+      src: "./video/01.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/02.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/03.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/04.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/05.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/06.mp4",
+      title: "Video",
+    },
+    {
+      src: "./video/07.mp4",
+      title: "Video",
+    },
+    {
+        src: "./video/08.mp4",
+        title: "Video",
+      },
+      {
+        src: "./video/09.mp4",
+        title: "Video",
+      },
+      {
+        src: "./video/10.mp4",
+        title: "Video",
+      },
+  ];
